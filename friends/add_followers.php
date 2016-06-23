1d2d793c267d9df366ff10939b7ae1bc906f9148
littleguga <fed777os@gmail.com>
2016-06-24 02:57:27 +0300

    fix add_followers when followers.txt was created by get_followers.php

--- friends/add_followers.php
+++ friends/add_followers.php
@@ -11,9 +11,16 @@ $result = json_decode(file_get_contents("followers.txt"), true)["response"];
 //empty log var
 $log = "";
 
+//if it followers
+if(isset($result["count"])){
+    $result = $result["items"];
+}else{
+    //if it friends requests
+}
+
 //Run through all received ids
 foreach ($result as $id) {
-    //send request to VK and add result to log 
+    //send request to VK and add result to log
     $log .= file_get_contents(getVkUrl($method, "user_id=" . $id, $token));
 
     //Sleep 1 sec
